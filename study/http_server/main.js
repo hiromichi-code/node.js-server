bae4617a38d7068216715012cfdf2481fe64c4d2
Hiro <hiromichi.s.617@gmail.com>
2021-02-04 13:31:24 +0900

    node.jsテスト

---
+++ study/http_server/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {　
+    "Content-Type": "text/html",
+  });
+
+  const responseMessage = "<h1>Hello Server!</h1>";
+  response.end(responseMessage);
+  console.log(`Sent a response : ${responseMessage}`);
+});
+
+server.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
